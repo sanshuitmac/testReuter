d614c72e7181d87fa0d987e37da310be935ba84a
Tracy <275612445@qq.com>
2025-02-27 00:19:21 +0800

    路透

--- rt.py
+++ rt.py
@@ -4,9 +4,9 @@ import time
 # from selenium import webdriver
 # from selenium.webdriver.chrome.service import Service
 # from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.by import By
 # from webdriver_manager.chrome import ChromeDriverManager
-import undetected_chromedriver as uc
+# import undetected_chromedriver as uc
 import requests
 import cloudscraper
 # from bs4 import BeautifulSoup
